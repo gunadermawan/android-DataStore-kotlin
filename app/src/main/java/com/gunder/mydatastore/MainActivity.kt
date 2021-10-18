c4ea910ee30c116d447fd9a68bed925217371ee4
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-18 19:57:11 +0700

    dependency preferences dataStore

--- app/src/main/java/com/gunder/mydatastore/MainActivity.kt
+++ app/src/main/java/com/gunder/mydatastore/MainActivity.kt
@@ -2,10 +2,28 @@ package com.gunder.mydatastore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.gunder.mydatastore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+//    binding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        inisialisasi button
+        binding.switchOn.setOnCheckedChangeListener { _ : CompoundButton, isChecked: Boolean ->
+            if (isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.switchOn.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.switchOn.isChecked = false
+            }
+        }
     }
 }
+
